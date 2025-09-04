55376fe3e381957bf91ad953df4b8913f516a5c4
Nithin Chalapathi <nithin.ch10@gmail.com>
2025-09-04 13:01:18 -0700

    runner updates

--- dev_train.sh
+++ dev_train.sh
@@ -20,5 +20,4 @@ echo $(which python)
 PYTORCH_ALLOC_CONF="expandable_segments:True" \
 TORCHFT_LIGHTHOUSE=${TORCHFT_LIGHTHOUSE} \
 torchrun --nproc_per_node=${NGPU} --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
---local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
 -m ${TRAIN_FILE} --job.config_file ${CONFIG_FILE} "$@"
